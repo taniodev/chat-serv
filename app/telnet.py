f4abf67c0cd9db14e6e17c3868746994ff0882bb
Tânio Scherer <tanioms3@gmail.com>
2019-12-14 17:38:01 -0300

    Refatora o código da aplicação e os testes

--- app/telnet.py
+++ app/telnet.py
@@ -1,19 +1,21 @@
 
+from app.mensageiro import Mensageiro
+
 conexoes = []
 
 
 async def telnet(reader, writer):
     conexoes.append(writer)
+    msg = Mensageiro(conexoes)
 
     while True:
         data = await reader.read(1024)
+        data = data.decode()
 
-        if 'sair' in data.decode():
+        if 'sair' in data:
             break
 
-        for conexao in conexoes:
-            conexao.write(data)
-            await conexao.drain()
+        await msg.enviar_para_todos(data)
 
     conexoes.remove(writer)
     writer.close()
--- tests/test_servidor_telnet.py
+++ tests/test_servidor_telnet.py
@@ -1,9 +1,58 @@
+from unittest.mock import AsyncMock, call, Mock
 
-def test_eco_da_mensagem(cliente_telnet1):
-    cliente_telnet1.send(b'testando')
-    assert b'testando' in cliente_telnet1.recv(1024)
+import pytest
+
+from app.telnet import conexoes, telnet
 
 
-def test_mensagem_para_todos_os_clientes(cliente_telnet1, cliente_telnet2):
-    cliente_telnet1.send(b'testando')
-    assert b'testando' in cliente_telnet2.recv(1024)
+def criar_writer_mock() -> AsyncMock:
+    """Cria e prepara um objeto writer mock para os testes."""
+    writer_mock = AsyncMock()
+
+    # Um objeto AsyncMock que não é chamado com await, faz com que apareça warning ao rodar os testes
+    writer_mock.write = Mock()
+
+    return writer_mock
+
+
+@pytest.fixture
+def writer_mock_interno() -> AsyncMock:
+    """Cria um objeto que representa um cliente já conectado ao servidor."""
+    writer_mock = criar_writer_mock()
+    conexoes.append(writer_mock)
+    yield writer_mock
+    conexoes.remove(writer_mock)
+
+
+@pytest.mark.asyncio
+async def test_eco_da_mensagem():
+    mensagens = [b'teste']
+    calls = [call(b'teste\n')]
+
+    writer_mock = criar_writer_mock()
+    reader_mock = AsyncMock()
+    reader_mock.read.side_effect = mensagens
+
+    try:
+        await telnet(reader_mock, writer_mock)
+    except RuntimeError:
+        pass
+
+    writer_mock.write.assert_has_calls(calls)
+
+
+@pytest.mark.asyncio
+async def test_mensagem_para_todos_os_clientes(writer_mock_interno):
+    mensagens = [b'teste']
+    calls = [call(b'teste\n')]
+
+    writer_mock = criar_writer_mock()
+    reader_mock = AsyncMock()
+    reader_mock.read.side_effect = mensagens
+
+    try:
+        await telnet(reader_mock, writer_mock)
+    except RuntimeError:
+        pass
+
+    writer_mock_interno.write.assert_has_calls(calls)
